a3c3b833ff873c9eacb3d1e131ce84d7cdc8da02
siddhp123 <47033295+siddhp123@users.noreply.github.com>
2023-10-10 18:58:36 -0400

    Added functionality for time setting buttons

--- javascript.js
+++ javascript.js
@@ -1,28 +1,112 @@
-const container = document.querySelector('#container');
+// Add settings with three settings
+// 1. Colour scheme
+// 2. duration of each timer
+// 3. configuration for pomodoro technique
+// 3.5 maybe presets for the configs?
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+let hours = 0; 
+let minutes = 1;
+let seconds = 10;
 
-container.appendChild(content);
+const pomoButton = document.getElementById("pomodoro");
+const shortButton = document.getElementById("short");
+const longButton = document.getElementById("long"); 
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', function (e) {
-    e.target.style.background = 'blue';
+pomoButton.addEventListener('click', () => {
+  setTime("pomodoro");
 });
 
-const btn2 = document.querySelector('#btn2');
-btn2.addEventListener('click', () => {
-  alert("Hello World");
+shortButton.addEventListener('click', () => {
+  setTime("short");
 });
 
-const buttons = document.querySelectorAll('button');
-
-// we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
-
-  // and for each one we add a 'click' listener
-  button.addEventListener('click', () => {
-    alert(button.id);
-  });
+longButton.addEventListener('click', () => {
+  setTime("long");
 });
+
+function setTime(id) {
+  // Check if the timer is running
+  // if it is stop it and then set time
+  
+  switch(id) {
+    case "pomodoro": {
+      hours = 1;
+      minutes = 0;
+      seconds = 30;
+      break; 
+    }
+    case "short": {
+      hours = 0;
+      minutes = 10;
+      seconds = 0;
+      break; 
+    }
+    case "long": {
+      hours = 0;
+      minutes = 20;
+      seconds = 0;
+      break; 
+    }
+  }
+  updateTimerDisplay();
+}
+
+let timerInterval;
+let timerRunning = false;
+
+const timerDisplay = document.getElementById("timertext");
+const toggleButton = document.getElementById("startstop");
+
+// Maybe add seconds countdown?
+function updateTimerDisplay() {
+  if (hours >= 1) {
+    timerDisplay.textContent = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  }
+  else if (minutes >= 1) {
+    timerDisplay.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  }
+  else {
+    timerDisplay.textContent = `${String(seconds).padStart(1, '0')}`;
+  }
+}
+
+function toggleTimer() {
+    if (timerRunning) {
+        clearInterval(timerInterval);
+        timerRunning = false;
+        toggleButton.textContent = "Start";
+    } else {
+        timerRunning = true;
+        toggleButton.textContent = "Stop";
+
+        timerInterval = setInterval(function () {
+            if (seconds === 0) {
+                if (minutes === 0) {
+                  if (hours === 0) {
+                    clearInterval(timerInterval);
+                    timerRunning = false;
+                    toggleButton.textContent = "Start";
+                    // Add a notification/alert here
+                    // Add functionality to automatically switch page state to the next timer
+                    alert("Timer Ended");
+                  }
+                  else {
+                    hours--;
+                    minutes = 59;
+                    seconds = 59;
+                  }
+                } else {
+                    minutes--;
+                    seconds = 59;
+                }
+            } else {
+                seconds--;
+            }
+
+            updateTimerDisplay();
+        }, 1000);
+    }
+}
+
+toggleButton.addEventListener("click", toggleTimer);
+updateTimerDisplay();
